cf64c5bf03cc017cc6db49a9c9ba3869bde31de2
cemorta <cem.orta@hotmail.com>
2025-04-02 18:26:00 +0300

    List admins who are responsible for an apartment

--- src/app/apartment-detail/apartment-detail.component.ts
+++ src/app/apartment-detail/apartment-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { ApartmentService } from '../services/apartment.service';
 import {Apartment} from '../models/apartment.model';
+import {ApartmentOwnership} from '../models/apartment-ownership.model';
 
 @Component({
   selector: 'app-apartment-detail',
@@ -12,6 +13,29 @@ import {Apartment} from '../models/apartment.model';
 export class ApartmentDetailComponent implements OnInit {
   apartmentId!: number;
   apartment?: Apartment;
+  showAdminsList = true; // Set this to true/false to show/hide the admins list
+  // TODO: This should be controlled by user role permission
+
+  // Method to sort ownerships by isActive (active first) and startDate (newest first)
+  sortedOwnerships(): ApartmentOwnership[] {
+    if (!this.apartment || !this.apartment.ownerships) {
+      return [];
+    }
+    console.log(this.apartment?.id);
+    console.log(JSON.stringify(this.apartment));
+
+    return [...this.apartment.ownerships].sort((a, b) => {
+      // Sort by active first (active comes first)
+      if (a.active !== b.active) {
+        return a.active ? -1 : 1;
+      }
+
+      // Then sort by startDate (newer dates first)
+      const dateA = new Date(a.startDate).getTime();
+      const dateB = new Date(b.startDate).getTime();
+      return dateB - dateA;
+    });
+  }
 
   constructor(
     private route: ActivatedRoute,
