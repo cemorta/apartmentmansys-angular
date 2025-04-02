a33a02ccc002fcc8dd31768cac8d23b8b0e68002
cemorta <cem.orta@hotmail.com>
2025-04-02 17:26:10 +0300

    Don't allow for analytics and modify the backend port

--- src/app/services/apartment.service.ts
+++ src/app/services/apartment.service.ts
@@ -7,7 +7,7 @@ import { Apartment } from '../models/apartment.model';
   providedIn: 'root'
 })
 export class ApartmentService {
-  private apiUrl = 'http://localhost:8080/api/apartments';
+  private apiUrl = 'http://localhost:8081/api/apartments';
 
   constructor(private http: HttpClient) {}
 
